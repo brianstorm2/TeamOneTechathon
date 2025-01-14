e669b9392ef149439dfba594b631f2650afd1a76
brianstorm2 <matt.j.parsons99@gmail.com>
2025-01-14 15:37:21 +0000

    Add Website Framework

---
+++ FinancialsForYou/src/main/java/com/example/financialsforyou/FinancialsForYouApplication.java
@@ -0,0 +1,21 @@
+package com.example.financialsforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication
+public class FinancialsForYouApplication {
+
+    @GetMapping("/") //website home page
+    public String index() {
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialsForYouApplication.class, args);
+    }
+
+}
---
+++ FinancialsForYou/src/test/java/com/example/financialsforyou/FinancialsForYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financialsforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialsForYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
